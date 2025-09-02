12e357b3c6f2041fb5cd3df91ebfbb9984e61a78
Higorhrs <higorrosa677@gmail.com>
2025-09-02 07:28:24 -0300

    Primeiro commit: projeto Conta3 inicial

---
+++ model/Conta.java
@@ -0,0 +1,49 @@
+package model;
+
+public class Conta {
+	private int numeroConta;
+	private String nomeTitular;
+	private double saldo;
+	
+	public Conta() {
+	}
+	
+	public Conta(int numeroConta, String nomeTitular, double saldo) {
+		this.numeroConta = numeroConta;
+		this.nomeTitular = nomeTitular;
+		this.saldo = saldo;
+	}
+	
+	public Conta(int numeroConta, String nomeTitular) {
+		this.numeroConta = numeroConta;
+		this.nomeTitular = nomeTitular;
+	}
+
+
+	public int getNumeroConta() {
+		return numeroConta;
+	}
+
+
+	public String getNomeTitular() {
+		return nomeTitular;
+	}
+
+
+	public void setNomeTitular(String nomeTitular) {
+		this.nomeTitular = nomeTitular;
+	}
+
+
+	public double getSaldo() {
+		return saldo;
+	}
+
+	public String toString() {
+		return "Conta: " + numeroConta 
+				+ " - Titular: " 
+				+ nomeTitular 
+				+ " - Saldo R$" 
+				+ saldo;
+	}
+}
---
+++ repository/ContaRepository.java
@@ -0,0 +1,26 @@
+package repository;
+
+import model.Conta;
+
+public class ContaRepository {
+	
+	Conta conta; 
+	
+	public void criarConta(int numeroConta, String nomeTitular) {
+		conta = new Conta();
+	}
+	public void depositar() {
+		
+	}
+	public void sacar() {
+		
+	}
+	public void alterar() {
+	
+	}
+	public void excluirConta() {
+		
+	}
+	
+
+}
